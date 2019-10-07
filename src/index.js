0321f003cd37d6683584e581eee967755b7fe189
Bruno Silva <bruno.tgasilva12@gmail.com>
2019-10-07 16:41:44 -0400

    Adicionado a SafeAreaView

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, SafeAreaView } from 'react-native';
 
 // import { Container } from './styles';
 
 export default function App() {
   return (
-    <View />
+    <SafeAreaView>
+      <View/>
+    </SafeAreaView>
   );
 }
