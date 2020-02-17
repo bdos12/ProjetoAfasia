6aa012cf44b529628fb1fdc3e2c497a73034c409
bdos12 <bruno.tgasilva12@gmail.com>
2020-02-17 12:13:01 -0400

    Correção na renderização dos itens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,13 +5,14 @@ import deleteItem from './deleteItem'
 
 module.exports = class HomePage extends Component {
 
-  static navigationOptions = {
+  static navigationOptions = { //Propriedades da navegação
     title: 'Inicio'
   }
 
   constructor(props){
     super(props);
     this.state = {
+      text: '',
       nameCategory: '',
       idCategory: 0,
       isCategory: true,
@@ -34,44 +35,77 @@ module.exports = class HomePage extends Component {
     return true;
   };
 
-  async loadRealm (){
+  async loadRealm (){ //Carregar itens do banco de dados
     const realm = await getRealm()
     const data = realm.objects('Category').sorted('id', true);
-    this.setState({data:data})
+
+    const add = {
+      id: Math.random() + 100,
+      name: 'Adicionar', 
+      uri: require('../../icons/add-circle-512.png'),
+      isAdd: true,
+    }
+   
+    const itens = []
+    itens.push(add)
+    itens.push(... data)
+
+    this.setState({data:itens})
     return data;
+
   }
   
-  setDate = (item) => {
-    this.setState({idCategory: item.id})
-    this.setState({nameCategory: item.name})
-    this.setState({data: this.state.data[this.state.data.findIndex((obj) => obj.id === item.id)].images})
-    this.setState({isCategory: false})
-  }
+  setDate = (item) => { //Alterar o estado e renderizar as imagens
+    let temp = this.state.data[this.state.data.findIndex((obj) => obj.id === item.id)].images 
 
-  addTTS = (obj) => {
-
-    const newData = {
-      id: Math.random() * 10,
-      name: obj.name,
-      uri: obj.uri,
-      idCategory: obj.idCategory,
-      isCategory: false
+    let data = []
+    const add = {
+      id: Math.random() + 100,
+      name: 'Adicionar', 
+      uri: require('../../icons/add-circle-512.png'),
+      isAdd: true,
     }
 
-    let tempImage = this.state.imagesTTS;
-    tempImage.push(newData)
+    data.push(add)
+    data.push(... temp)
 
-    this.setState({imagesTTS: tempImage})
 
+    this.setState({idCategory: item.id})
+    this.setState({nameCategory: item.name})
+    this.setState({data: data})
+    this.setState({isCategory: false})
   }
-
-  handleDeleteItem = (item) => {
+  
+  handleDeleteItem = (item) => { //Deletar item do Banco de dados
     deleteItem(item)
     this.loadRealm()
   }
   
-  renderItem = ({item}) => {
-    if(item.isCategory){
+  handleAddItem = () => { //Navegação de telas entre addCategory e addImage
+    {
+      this.state.isCategory ?
+      this.props.navigation.navigate('AddCategory'):
+      this.props.navigation.navigate('AddImage', 
+      {
+        idCategory: this.state.idCategory,
+        nameCategory: this.state.nameCategory
+      })
+    }
+  }
+
+  renderItem = ({item}) => { //Rendeizar os itens da FlatList
+    if(item.isAdd){
+      return(
+        <>
+        <TouchableOpacity style = {{margin: 25, alignItems: 'center'}}
+        onPress = {() => this.handleAddItem()}
+        >
+          <Image style={{width: 130, height: 130}} source={require('../../icons/add-circle-512.png')}/>
+          <Text>{item.name}</Text>
+        </TouchableOpacity>
+        </>
+      )
+    }else if(item.isCategory){
       return(
         <>
         <TouchableOpacity onPress={() => {
@@ -80,10 +114,10 @@ module.exports = class HomePage extends Component {
         onLongPress = {() => Alert.alert(
           'Teste ', 
           'Apagar categoria ' + item.name + '?', 
-        [
-          {text: 'Sim', onPress: () => this.handleDeleteItem(item)},
-          {text: 'Não'}
-        ]
+          [
+            {text: 'Sim', onPress: () => this.handleDeleteItem(item)},
+            {text: 'Não'}
+          ]
         )} 
         delayLongPress ={300} 
         style={{margin: 25, width: 130, height: 130}}
@@ -111,7 +145,7 @@ module.exports = class HomePage extends Component {
       ]
       )} 
       delayLongPress = {300}
-
+      
       onPress = {() => this.addTTS(item)}
       style={{margin: 25, width: 130, height: 130}}
       >
@@ -125,26 +159,45 @@ module.exports = class HomePage extends Component {
       );
     }
   }
+  
+  addTTS = (obj) => { //Adicionar item na barra TTs
 
+    const newData = {
+      id: Math.random() * 10,
+      name: obj.name,
+      uri: obj.uri,
+      idCategory: obj.idCategory,
+      isCategory: false
+    }
 
-  renderTTS = obj => {
+    let text = this.state.text;
+    text = text + ' ' + newData.name
+    this.setState({text})
+    let tempImage = this.state.imagesTTS;
+    tempImage.push(newData)
+
+    this.setState({imagesTTS: tempImage})
+
+  }
+  
+  renderTTS = obj => { //Renderizar itens da barra TTs
     return(
       <View>
         <Image style = {{height: 60, width: 60, margin: 2}} 
         source = {{uri: obj.item.uri}}
         />
-        <Text>{obj.item.name}</Text>
+        {/* <Text>{obj.item.name}</Text> */}
       </View>
     );
   }
-
-  deleteTTs = () => {
+  
+  deleteTTs = () => { //Deletar itens da barra TTs
     let temp = this.state.imagesTTS;
     temp.pop();
     this.setState({imagesTTS: temp})
   }
   
-  render() {
+  render() { //Render principal
     return (
       <>
       <View style={{backgroundColor: '#ccc', width:'100%', height: 100, flexDirection: 'row'}}>
@@ -155,7 +208,8 @@ module.exports = class HomePage extends Component {
         renderItem = {this.renderTTS}
         keyExtractor = {(item) => String(item.id)}
         />
-        <View style={{height: '100%', width: '20%', backgroundColor: '#bbb', }}>
+        <Text>{this.state.text}</Text>
+        <View style={{height: '100%', width: '20%', backgroundColor: '#ccc', }}>
           <Button title='TTs' onPress={() => {}} />
           <TouchableOpacity onLongPress ={() => alert('Deseja apagar tudo?')} delayLongPress={3000}>
             <Button title='Apagar' onPress={() => this.deleteTTs()}/>
@@ -163,16 +217,6 @@ module.exports = class HomePage extends Component {
         </View>
       </View>
       <SafeAreaView style={styles.item}>
-        {this.state.isCategory
-        ?<TouchableOpacity style={styles.ButtonAdd} onPress={() => this.props.navigation.navigate('AddCategory')}>
-          <Image style = {styles.Images} source={require('../../icons/add-circle-512.png')}/>
-          <Text style={styles.TextAdd}>Adicionar categoria</Text>
-        </TouchableOpacity>
-        :<TouchableOpacity style={styles.ButtonAdd} onPress={() => this.props.navigation.navigate('AddImage', {idCategory: this.state.idCategory, nameCategory: this.state.nameCategory})}>
-          <Image style = {styles.Images} source={require('../../icons/add-circle-512.png')}/>
-          <Text style={styles.TextAdd}>Adicionar Imagem</Text>
-        </TouchableOpacity>
-        }
         <FlatList 
         data = {this.state.data}
         keyExtractor = {(item) => String(item.id)}
