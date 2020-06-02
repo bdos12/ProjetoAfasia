6fc93f05c4e4ebfffd0d74e0766ba5e74e70aaa3
Bruno Silva <bruno.tgasilva12@gmail.com>
2020-06-02 08:07:25 -0400

    Configuração do navigator para function component

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,16 @@ import HomePage from './pages/Home/index'
 const AppNavigator = createStackNavigator(
   {
     Home: {
-      screen: HomePage
+      screen: HomePage,
+      navigationOptions: {
+        title: "Inicio"
+      },
+      titleStyle:{
+        fontSize: 40,
+      },
+      headerStyle: {
+        backgroundColor: '#b8daf5',
+      },
     },
   },
   {
