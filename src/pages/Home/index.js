c621687516f64fef4cdf32824e05cb33a644c0bd
Bruno Silva <bruno.tgasilva12@gmail.com>
2020-06-02 08:07:52 -0400

    Reestruturação do código para functional component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,444 +1,89 @@
-import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  Alert,
-  BackHandler,
-  TouchableOpacity,
-  Image,
-  Text,
-  View,
-  TextInput,
-  Dimensions,
+import React, {useState, useEffect} from 'react';
+import { 
+  View, 
   FlatList, 
+  Text, 
+  TouchableOpacity, 
+  Alert, 
+  Image, 
+  Button
 } from 'react-native';
-import getRealm, { deleteItem, saveRealm }from '../../services/realm';
-import speak from '../../services/tts';
-import Modal from "react-native-modal";
-import ImagePicker from 'react-native-image-picker';
-import ImageResizer from 'react-native-image-resizer';
-import ImgToBase64 from 'react-native-image-base64';
+
+import getRealm, { deleteItem, saveRealm, loadRealm } from '../../services/realm'
 
 import styles from './styles'
 
-// let deviceWidth = Dimensions.get('window').width
+const HomePage = () => {
+  const [data, setData] = useState([])
 
+  useEffect(() => {
+    loadRealm(data)
+    .then(item => setData(item))
+    .catch(err => alert(err))
+  }, [])
 
-module.exports = class HomePage extends Component {
-  static navigationOptions = {
-    // Propriedades da navegação
-    title: 'INICIO',
-    titleStyle:{
-      fontSize: 40,
-    },
-    headerStyle: {
-      backgroundColor: '#b8daf5',
-    },
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      nome: ' ',
-      uri: ' ',
-      text: '',
-      nameCategory: ' ',
-      idCategory: 0,
-      positionCategory: 0,
-      isCategory: true,
-      data: [],
-      imagesTTS: [],
-      isVisible: false,
-      select: true,
-    };
-    this.loadRealm();
-  }
-
-  componentDidMount() {
-    this.BackHandler = BackHandler.addEventListener(
-      'hardwareBackPress', () => 
-      {
-        this.loadRealm()
-        this.setState({isCategory: true})
-        return true
-      },
-    );
-  }
-
-  async loadRealm() {
-    // Carregar itens do banco de dados
-    const realm = await getRealm();
-
-    const add = {
-      id: Math.random() + 100,
-      name: 'Adicionar',
-      uri: require('./icons/add-circle-512.png'),
-      isAdd: true,
-    };
-
-    let itens = []
-    itens.push(add)
-    itens.push(...realm.objects('Category'))
-
-
-    this.setState({data: itens})
-
-  }
-
-  setDate = item => {
-    // Alterar o estado e renderizar as imagens
-    const temp = this.state.data[
-      this.state.data.findIndex(obj => obj.id === item.id)
-    ].images;
-
-    const data = [];
-    const add = {
-      id: Math.random() + 100,
-      name: 'Adicionar ',
-      uri: require('./icons/add-circle-512.png'),
-      isAdd: true,
-    };
-
-    data.push(add);
-    data.push(... temp);
-
-    this.setState({idCategory: item.id});
-    this.setState({positionCategory: this.state.data.findIndex(obj => obj.id === item.id)});
-
-    this.setState({nameCategory: item.name});
-    this.setState({data});
-    this.setState({isCategory: false});
-  };
-
-  async handleDeleteItem (item) {
-    // Deletar item do Banco de dados
-    await deleteItem(item);
-    this.loadRealm();
-  };
-
-  handleAddItem = () => {
-    this.setState({isVisible: true})
-  };
-
-  renderItem = ({item}) => { // Rendeizar os itens da FlatList
-    if (item.isAdd) {
-      return (
-        <View style={styles.ViewItens}>
+  function renderItem({item}){ //Falta o add Item
+    if(item.isCategory){
+      return(
+        <View style={styles.item}>
           <TouchableOpacity
-            onPress={() => this.handleAddItem()}>
-            <Image
-              style={styles.Images}
-              source={require('./icons/icon_add.png')}
-              />
-            <Text style={styles.textItens}>{item.name}</Text>
-
-          </TouchableOpacity>
-        </View>
-      );
-    }
-    if (item.isCategory) {
-      return (
-        <View style={styles.ViewItens}>
-          <TouchableOpacity
-            onPress={() => {
-              this.setDate(item);
-            }}
-            onLongPress={() =>
-              Alert.alert('Apagar ', `Apagar categoria ${item.name}?`, [
-                {text: 'Sim', onPress: () => this.handleDeleteItem(item)},
+            onPress={() => setItem(item)}
+            onLongPress={() => 
+              Alert.alert('Apagar', `Apagar categoria ${item.name}?`,[
+                {text: 'Sim', onPress: () => {}/* Deletar item*/},
                 {text: 'Não'},
-              ])
+              ])  
             }
             delayLongPress={300}>
-            <Image style={styles.Images} source={{uri: item.uri}} />
-            <Text style={styles.textItens}>{item.name}</Text>
-
+              <Image style={styles.images} source={{uri: item.uri}}/>
+              <Text style={styles.textItem}>{item.name}</Text>
           </TouchableOpacity>
-
         </View>
       );
     }
-    return (
-        <View style={styles.ViewItens}>
-          <TouchableOpacity
-            onLongPress={() =>
-              Alert.alert('Apagar ', `Apagar o item ${item.name}?`, [
-                {text: 'Sim', onPress: () => this.handleDeleteItem(item)},
-                {text: 'Não'},
-              ])
-            }
-            delayLongPress={300}
-            onPress={() => this.addTTS(item)}>
-            <Image style={styles.Images} source={{uri: item.uri}} />
-            <Text style={styles.textItens}>{item.name}</Text>
-
-          </TouchableOpacity>
-        </View>
-    );
-  };
-
-  addTTS = obj => { // Adicionar item na barra TTs
-    const newData = {
-      id: Math.random() * 10,
-      name: obj.name,
-      uri: obj.uri,
-      idCategory: obj.idCategory,
-      isCategory: false,
-    };
-
-    let {text} = this.state;
-    text = `${text} ${newData.name}`;
-    this.setState({text});
-    const tempImage = this.state.imagesTTS;
-    tempImage.push(newData);
-
-    this.setState({imagesTTS: tempImage});
-  };
-
-  renderTTS = obj => { // Renderizar itens da barra TTs
-    return (
-      <View style={styles.viewTTs}>
+    return(
+      <View style={styles.item}>
         <TouchableOpacity
-          onLongPress={item =>
-            Alert.alert('Apagar', 'Deseja apagar?', [
-              {text: 'Sim', onPress: () => this.deleteTTs(item)},
-              {text: 'Não'},
-            ])
-          }
-          delayLongPress={1500}>
-          <Image
-            style={styles.imagesTTS}
-            source={{uri: obj.item.uri}}
-            />
-          <Text style={styles.textItens}>{obj.item.name}</Text>
+        onLongPress={() => 
+          Alert.alert('Apagar', `Apagar imagem ${item.name}?`,[
+            {text: 'Sim', onPress: () => {}/* Deletar item*/},
+            {text: 'Não'},
+          ])  
+        }
+        delayLongPress={300}>
+          <Image style={styles.images} source={{uri: item.uri}}/>
+          <Text style={styles.textItem}>{item.name}</Text>
+
         </TouchableOpacity>
       </View>
     );
-  };
-
-  deleteTTs = obj => { // Deletar itens da barra TTs
-    const temp = this.state.imagesTTS;
-    if (obj) {
-      temp.pop();
-    } else {
-      temp.pop();
-    }
-    this.setState({imagesTTS: temp});
-  };
-
-  handleSpeak = () => {
-    const temp = this.state.imagesTTS;
-    let text = '';
-    temp.forEach(item => {
-      text = `${text} ${item.name}`;
-    });
-    speak(text);
-  };
-
-  chooseFile = (option) => { //Image Picker
-    var options = {
-      title: 'Selecionar Imagem',
-      takePhotoButtonTitle: 'Tirar uma foto',
-      chooseFromLibraryButtonTitle: 'Escolher da galeria',
-      quality: 1,
-      noData: true,
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-
-    if(option === 1){
-      ImagePicker.launchCamera(options, response => {
-        if (response.error) {
-          alert('ImagePicker Error: ', response.error);
-        } else {
-          if(response.path){
-            ImageResizer.createResizedImage(response.path, 800, 600, 'JPEG', 100)
-            .then(({uri}) => {
-                ImgToBase64.getBase64String(uri)
-                .then(base64String => {
-                  this.setState({uri: 'data:image/jpeg;base64,' + base64String})
-                }).catch(err => {alert(err)})
-            }).catch(err => alert(err))
-          }
-        }
-      });
-      this.setState({select: false})
-    }else{
-      ImagePicker.launchImageLibrary(options, response => {
-        if (response.error) {
-          alert('ImagePicker Error: ', response.error);
-        } else {
-          if(response.path){
-            ImageResizer.createResizedImage(response.path, 800, 600, 'JPEG', 100)
-            .then(({uri}) => {
-              ImgToBase64.getBase64String(uri)
-              .then(base64String => {
-                this.setState({uri: 'data:image/jpeg;base64,' + base64String})
-
-              }).catch(err => alert(err))
-            }).catch(err => alert(err))
-          }
-        }
-      });
-      this.setState({select: false})
-    }
-  };
-
-  handleCancelModal = () => { //Cancelar o modal
-    this.setState({select: true})
-    this.setState({isVisible: false})
-    this.setState({uri: ' '})
-    this.setState({name: ''})
   }
 
-  async handleSaveRealm() { //Salvar no banco de dados
-    let item = {
-      name: this.state.name,
-      uri: this.state.uri,
-      idCategory: this.state.idCategory
-    }
-    if (item.name === ' ' || item.uri === ' '){
-      return Alert.alert("Erro ao adicionar!", "Precisa adicionar uma imagem e um nome.")
-    }
-    if (this.state.isCategory){
-      if(await saveRealm(item, 1, this.state.positionCategory)){
-        this.setState({name: ' '})
-        this.setState({uri: ' '})
-        this.loadRealm()
-        this.handleCancelModal()
-      }
-    }else {
-      if(await saveRealm(item, 2, this.state.positionCategory)){
-        this.setState({name: ' '})
-        this.setState({uri: ' '})
-        this.loadRealm()
-        this.handleCancelModal()
-      }
-    }
+  function setItem(item){
+
+    setData(data[data.findIndex(obj => obj.id === item.id)].images)
   }
-  
-  render() { //Render princial
-    const columns = 4;
-    return (
-      <>
-      <Modal isVisible = {this.state.isVisible}
-      style={{
-        maxHeight: 500,
-        maxWidth: 500,
-        alignSelf: 'center',}}
-        onBackButtonPress = {() => this.handleCancelModal()}
-        >
-          
-        <View style={styles.container}>
-        {
-          this.state.select ? 
-        <View>
-          <View style={styles.viewTitle}>
-            <Text style={styles.viewTitleText}>Adicionar</Text>
-          </View>
-          <View style={styles.viewMiddle}>
-            <View style={styles.viewMiddleIcon} >
-              <TouchableOpacity onPress={() => this.chooseFile(1)}>
-                <Image style ={styles.iconImage} source={require('./icons/icon_camera.png')} />
-              </TouchableOpacity>
-                <Text style={styles.middleIconText} >Câmera</Text>
-            </View>
-            <View style={styles.viewMiddleIcon}>
-              <TouchableOpacity onPress={() => this.chooseFile(2)}>
-                <Image style ={styles.iconImage} source={require('./icons/icon_gallery.png')} />
-              </TouchableOpacity>
-                <Text style={styles.middleIconText}>Galeria</Text>
-            </View>
-          </View>
-          <View style={styles.viewBottom}>
-              <TouchableOpacity style = {styles.viewBottomItem} onPress = {() => this.handleCancelModal()}>
-                <Text style={styles.textadd}>Cancelar</Text>
-              </TouchableOpacity>
-          </View>
-        </View> 
-        : //Se o Select for falso
-        <View style={styles.container}>
-          <View style={styles.viewTitle}>
-                <Text style={styles.AddCatg}>Adicionar</Text>
-          </View>
-          <View style={styles.viewMiddle}>
-            <View style = {styles.viewMiddleItem}>
-              <View style={{alignItems:'center'}}>
-                <View style={styles.viewImage}>
-                  <Image style={styles.image} source = {{uri: this.state.uri}}/>
-                </View>
-              </View>
-              <View style = {styles.viewMiddleInput}>
-                <Text style={styles.AddNome}>Nome: </Text>
-                <TextInput 
-                style={styles.input}
-                value={this.state.name} 
-                onChangeText={text => this.setState({name: text})}
-              />
-              </View>
-            </View>
-          </View>
-          <View style={styles.viewBottom}>
-              <TouchableOpacity style = {styles.viewBottomItem} onPress = {() => this.handleCancelModal()}>
-                <Text style={styles.AddCatg}>Cancelar</Text>
-              </TouchableOpacity>
-            <TouchableOpacity style = {styles.viewBottomItem} onPress={() => this.handleSaveRealm()}>
-                <Text style={styles.AddCatg}>Adicionar</Text>
-              </TouchableOpacity>
-          </View>
-        </View>
-        }
+
+  const columns = 4
+  return (
+    <View style={styles.container}> 
+      {/* View Principal */}
+      <View style={styles.viewTTS}>
+        {/* View TTS */}
+
       </View>
-      </Modal>
-      <>
-        <View style={styles.TTs}>
-          <FlatList
-            style={styles.flatListTTS}
-            data={this.state.imagesTTS}
-            extraData={this.state}
-            horizontal
-            renderItem={this.renderTTS}
-            keyExtractor={item => String(item.id)}
-            />
-          <View style={styles.TTsView}>
-            <TouchableOpacity onPress={() => this.handleSpeak()}>
-              <Image
-                style={styles.iconsTTS}
-                source={require('./icons/play.png')}
-              />
-            </TouchableOpacity>
 
-            <TouchableOpacity
-              onPress={() => this.deleteTTs()}
-              onLongPress={() =>
-                Alert.alert('Apagar', 'Deseja apagar tudo?', [
-                  {text: 'Sim', onPress: () => this.setState({imagesTTS: []})},
-                  {text: 'Não'},
-                ])
-              }
-              delayLongPress={1500}>
-              <Image
-                style={styles.iconsTTS}
-                source={require('./icons/exclu.png')}
-                />
-            </TouchableOpacity>
-          </View>
-        </View>
-        <SafeAreaView style={styles.item}>
-          <FlatList
-            style={styles.flatlistItem}
-          // createRows(this.state.data, columns)
-            // data = {this.createRows(this.state.data, columns)}
-            data={this.state.data}
-            keyExtractor={item => String(item.id)}
-            renderItem={this.renderItem}
-            extraData={this.state}
-            numColumns={columns}
-            />
-        </SafeAreaView>
-      </>
-      </>
-    );
-  }
-};
+      <View style={styles.viewItens}>
+        {/* Views corpo */}
+        <FlatList 
+          data ={data}
+          renderItem = {renderItem}
+          numColumns={columns}
+          keyExtractor = {(item) => String(item.id)}
+        />
+      </View>
+    </View>
+  );
+}
+
+export default HomePage;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,227 +3,57 @@ import { StyleSheet, Dimensions } from 'react-native'
 let deviceWidth = Dimensions.get('window').width
 let deviceHeight = Dimensions.get('window').height
 
-
 const styles = StyleSheet.create({
-    ViewItens: { //filtro de sobre a imagem
-      width: deviceWidth * 0.15,
-      height: deviceHeight * 0.4,
-      flex: 1/4,
-      margin: '2%',
-      borderRadius: 10,
-      // alignItems: 'center',
-      //borderWidth: 10,
-      borderColor: '#fff',
-      //backgroundColor: '#DCEDFA',
-    },
-    textItens : { //Estilização de texto
-      textAlign: 'center',
-      alignItems: 'center',
-      alignContent: 'center',
-      fontSize: deviceWidth * 0.015,
-      margin: '5%'
-      // resizeMode:'contain',
-      // tex
-      // fontSize: deviceWidth * 0.02,
-
-    },
-    viewTTs:{
-      height: deviceHeight * 0.30,
-      width: deviceWidth * 0.13,
-
-    },
-    imagesTTS: {// Conteiner das imagens colocadas na caixa de texto
-      // width: 150,
-      width: deviceWidth * 0.12,
-      height: deviceHeight * 0.15,
-      resizeMode:'contain',
-      margin: 8,
-      // backgroundColor: '#f5f',
-    },
-    iconsTTS: { //Conteiner dos icones de play e exclusão
-     margin: 2,
-     width: deviceWidth * 0.1, 
-     height: deviceHeight * 0.14,
-     marginTop: -1,
-     resizeMode:'contain'
-    //  backgroundColor: '#2ff',
-    },
-    item: { // Conteiner abaixo da caixa de texto
-      flex: 1,
-      flexDirection: 'column',
-      flexBasis: 0,
-      backgroundColor: '#b8daf5',
-      width: deviceWidth,
-    },
-    Images: {
-      // width: 200,
-      top: 10,
-      width: '100%',
-      height: '70%',
-      resizeMode:'contain',
-     borderRadius: 10,
-     shadowRadius: 20,
-    
+  container:{ //Container principal
+    // alignItems: 'center'
+    height: deviceHeight,
+    width: deviceWidth,
+    backgroundColor: "#e1e1e1",
+    flexDirection: 'column'
+  },
+  viewTTS: { //Container do TTS
+    height: deviceHeight * 0.3,
+    backgroundColor: "#c1c1c1",
+    // borderColor: '#000',
+  },
+  viewItens:{ //Container dos itens
+    width: '100%',
+    // height: '100%',
+    // backgroundColor: "#0f0",
+    flex: 1,
+    // alignItems: 'baseline'
+  },
+  item: { //Item flatList > viewItens
+    width: deviceWidth * 0.15,
+    height: deviceHeight * 0.4,
+    flex: 1/4,
+    borderRadius: 10,
+    borderColor: '#fff',
+  },
+  images: { // Imagens > RenderItem > flatList > viewItens
+    top: '10%',
+    width: '100%',
+    height: '70%',
+    resizeMode:'contain',
+    borderRadius: 10,
+    shadowRadius: 20,
     borderColor:'#DCEDFA',
-    },
-    TTs: { //conteiner  da caixa de texto
-      backgroundColor: '#fff',
-      width: deviceWidth,
-      height: deviceHeight * 0.30,
-      flexDirection: 'row',
-      borderColor: '#5fa2d4',
-      borderWidth: 5,
-      overflow: 'visible',
-    },
-    TTsView: {//Conteiner de texto do lado direito
-      height: '100%',
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'flex-end',
-    },
-    container: { //Alerta de adicionar imagem  
-      height: deviceHeight * 0.7,
-      width: deviceWidth * 0.4,
-      backgroundColor: '#88C7F6',
-      borderColor: '#fff',
-      borderWidth: 6,
-      borderRadius: 3,
-    },
-    viewTitle: { //conteirner superior do alert de add imagem
-      height: deviceHeight * 0.1,
-      alignItems:'center',
-      justifyContent: 'center',
-      borderColor: '#5794C2',
-      borderWidth: 3,
-      borderRadius: 3,
-      backgroundColor: '#B8DAF5'
-       
-       /*#B8DAF5*/
-       /*#5794C2 */ 
-  
-     
-    },
-    viewTitleText: { // Titulo do conteiner de add imagem
-      fontSize: deviceWidth * 0.02,
-      marginTop: '10%', 
-      marginBottom: '10%'
-    },
-    viewMiddle: { //conteiner do meio do alert de add imagem
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: deviceHeight * 0.47,
-      borderColor: '#5794C2',
-      borderWidth: 3,
-      borderRadius: 3,
-      backgroundColor: '#B8DAF5'
-    },
-    viewBottom: { //conteiner inferior do alert de add imagem
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center' ,
-      height: deviceHeight * 0.1,
-      borderColor: '#5794C2',
-      borderWidth: 3,
-      borderRadius: 3,
-      backgroundColor: '#B8DAF5'
-     
-    },
-    viewMiddleIcon:{ //conteiner dos icones do alert de add imagens
-      alignItems: 'center',
-      margin: '2%',
-      // backgroundColor: '#fa0'
-    },
-    middleIconText: {
-      fontSize: deviceWidth * 0.03,
-    },
-   
-    textadd:{// Texto conteiner inferior do alert de add imagem - interno
-      fontSize: deviceWidth * 0.02,
-      
-    },
-    input: {///Contorno do conteiner de texto de add categoria
-      width: deviceWidth * 0.15,
-      height: deviceHeight * 0.1,
-      fontSize: deviceWidth * 0.02,
-      marginBottom: -3,
-      backgroundColor: '#fff'
-    },
-    viewBottomIcon: {
-      alignContent: 'center',
-      alignItems: 'center',
-      margin: '2%',
-      width: 300,
-      height: 300,
-       //backgroundColor: '#a2fa'
-    },
-    iconImage: {//Conteiner por de tras dos icones de adicionar imagem
-      height: deviceHeight * 0.25,
-      width: deviceWidth * 0.15,
-      backgroundColor:'#DCEDFA',
-      resizeMode:"contain",
+  },
+  textItem: { //Text Itens > RenderItem > flatList > viewItens
+    textAlign: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    fontSize: deviceWidth * 0.015,
+    top: '10%'
+  },
 
 
-    },
-    image:{// Add imagem--> Conteiner por de tras da imagem adicionada
-      marginTop: '10%',
-      height: deviceHeight * 0.25,
-      width: deviceWidth * 0.15,
-      backgroundColor:'#fff' ,
-      resizeMode:"contain",
-    },
-    viewImage: {// Add imagem--> Conteiner por de tras da imagem adicionada 
-      height: deviceHeight * 0.25,
-      width: deviceWidth * 0.15,
-       //backgroundColor: '#5abc'
-    },
-    viewMiddleInput: { // conteiner do nome da imagem -> Add
-      flexDirection: 'row',
-      justifyContent: 'flex-start',
-      alignItems: 'center',
-      height: deviceHeight,
-      width: deviceWidth * 0.295,
-      flex: 1,
-      marginTop: '5%',
-      //  backgroundColor: '#22fdab'
-    },
-    viewMiddleItem: { //Add imagem - Conteiner ao redor da  imagem adicionada da galeria
-       //backgroundColor: '#fabd',
-      alignItems: 'center',
-      height: deviceHeight * 0.40,
-      width: deviceWidth * 0.35,
-      resizeMode:"contain",
-    },
-    viewBottomItem: {//Cor do conteiner de cancelar de add imagem 
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: deviceWidth * 0.40,
-      width: deviceWidth * 0.3,
-       //backgroundColor: '#fabc'
-    },
-    AddCatg:{ //Fontes de --> Adicionar Categoria/ Cancelar/ Adicionar Obs: Depois de selecionar a imagem
-      fontSize: deviceWidth * 0.02,
-    },
-    AddNome:{
-      fontSize: deviceWidth * 0.02,
-    },
-    flatListTTS:{
-      width: deviceWidth - 300,
-      height: deviceHeight * 0.30,
-    },
-    modalAdd: {
-      maxHeight: deviceHeight * 0.2,
-      maxWidth: deviceWidth * 0.2,
-      alignSelf: 'center',
-    },
-    itemEmpty: {
-      backgroundColor: "transparent"
-    },
-    flatlistItem:{
-      // height:deviceHeight + 500,
-      // width: deviceWidth
-    }
-  });
+
+
+  teste:{
+    height: deviceHeight,
+    width: deviceWidth
+  }
+})  
   
 export default styles;
